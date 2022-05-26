72bc3d1f869a5b7eb9b32404a11b90d8306662f9
Sumit Dadwal <sumitdadwal1@gmail.com>
2022-05-26 13:27:59 -0700

    white noise, static root

--- ecommerceapp/settings.py
+++ ecommerceapp/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ecommerceapp.urls'
@@ -124,6 +126,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static")
     ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
