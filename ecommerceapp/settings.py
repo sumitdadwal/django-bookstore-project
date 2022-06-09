d221399f060576de21ef5e13b1be055afcb1be28
Sumit Dadwal <sumitdadwal1@gmail.com>
2022-06-08 21:16:04 -0400

    allowed host change

--- ecommerceapp/settings.py
+++ ecommerceapp/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['betterbookstore.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['murmuring-oasis-10953.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
